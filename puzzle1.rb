4c2e499d82283f64de676267a9eee4eca5c8beb7
chitrank-samaiya <chitranksamaiya@yahoo.com>
2016-07-28 00:05:07 +0530

    refactoring using rubocop

--- puzzle1.rb
+++ puzzle1.rb
@@ -1,53 +1,53 @@
-def displayPathtoPrincess(n, grid)
-  raise "Number should be an odd number" if n.even?
-  raise "Number should be greater and equal to 3 and less than to 100"  if n < 3 || n > 100
+def display_path_to_princess(size_grid, grid)
+  raise 'Number should be an odd number' if size_grid.even?
+  raise 'Number should be between (3 >= n < 100)' if size_grid < 3 || size_grid > 100
 
-  b_x = b_y = get_bot_position(n, grid)
-  p_x, p_y = get_princess_corner(n, grid)
-
-  bot_moving_towards_princess(b_x, b_y, p_x, p_y)   	
+  bot_x = bot_y = get_bot_position(size_grid, grid)
+  princess_x, princess_y = get_princess_corner(size_grid, grid)
+  bot_moving_towards_princess(bot_x, bot_y, princess_x, princess_y)
 end
 
-#bot postion by cordinates X-axis and Y-axis
-def get_bot_position(n, grid)
-  center_position = n/2
-  raise "Position of bot is not in center" unless grid[center_position][center_position] == 'm'
-  return center_position 
+# bot postion by cordinates X-axis and Y-axis
+def get_bot_position(size_grid, grid)
+  center = size_grid / 2
+  raise 'Position of bot is not in center' unless grid[center][center] == 'm'
+  center
 end
 
-#princess postion by cordinates X-axis and Y-axis
-def get_princess_corner(n, grid)
-  return 0, 0     if grid[0][0] == 'p'
-  return 0, n-1   if grid[0][n-1] == 'p' 
-  return n-1, 0   if grid[n-1][0] == 'p' 
-  return n-1, n-1 if grid[n-1][n-1] == 'p'
-  raise "Oops bot not able to find princess in corners"
+# princess postion by cordinates X-axis and Y-axis
+def get_princess_corner(size_grid, grid)
+  return 0, 0 if grid[0][0] == 'p'
+  return 0, size_grid - 1 if grid[0][size_grid - 1] == 'p'
+  return size_grid - 1, 0 if grid[size_grid - 1][0] == 'p'
+  return size_grid - 1, size_grid - 1 if grid[size_grid - 1][size_grid - 1] == 'p'
+  raise 'Oops bot not able to find princess in corners'
 end
 
-#moving towards princess
-def bot_moving_towards_princess(b_x, b_y, p_x, p_y)
-  if b_x == p_x && b_y == p_y
-    return
-  else
-    if b_y < p_y
-      puts "Right"
-      b_y += 1
-    elsif b_y > p_y
-      puts "LEFT"
-      b_y -= 1
-    elsif b_x < p_x
-      puts "DOWN"
-      b_x += 1
-    elsif b_x > p_x
-      puts "UP"
-      b_x -= 1
-    end
-    bot_moving_towards_princess(b_x, b_y, p_x, p_y)
+# moving towards princess and recursive call
+def bot_moving_towards_princess(bot_x, bot_y, princess_x, princess_y)
+  return if bot_x == princess_x && bot_y == princess_y
+  if bot_y < princess_y
+    puts 'Right'
+    bot_y += 1
+  elsif bot_y > princess_y
+    puts 'LEFT'
+    bot_y -= 1
+  elsif bot_x < princess_x
+    puts 'DOWN'
+    bot_x += 1
+  elsif bot_x > princess_x
+    puts 'UP'
+    bot_x -= 1
   end
+  bot_moving_towards_princess(bot_x, bot_y, princess_x, princess_y)
 end
 
-#m denote bot and p denote princess inside grid
-n = 5
-grid = [['-','-','-','-','_'],['-','-','-','-','-'],['-','-','m','-','-'],['-','-','-','-','-'],['p','-','-','-','-']]
+# m denote bot and p denote princess inside grid
+size_grid = 5
+grid = [['-', '-', '-', '-', '_'],
+        ['-', '-', '-', '-', '-'],
+        ['-', '-', 'm', '-', '-'],
+        ['-', '-', '-', '-', '-'],
+        ['p', '-', '-', '-', '-']]
 
-displayPathtoPrincess(5, grid)
+display_path_to_princess(size_grid, grid)
--- puzzle2.rb
+++ puzzle2.rb
@@ -1,63 +1,59 @@
-def displayPathtoPrincess(n, grid)
-  raise "Number should be an odd number" if n.even?
-  raise "Number should be greater and equal to 3 and less than to 100"  if n < 3 || n > 100
+def display_path_to_princess(size_grid, grid)
+  raise 'Number should be an odd number' if size_grid.even?
+  raise 'Number should be between (3 >= n < 100)' if size_grid < 3 || size_grid > 100
 
-  b_x, b_y = get_bot_position(grid)
-  p_x, p_y = get_princess_position(grid)
-
-  bot_moving_towards_princess(b_x, b_y, p_x, p_y)   	
+  bot_x, bot_y = get_bot_position(grid)
+  princess_x, princess_y = get_princess_position(grid)
+  bot_moving_towards_princess(bot_x, bot_y, princess_x, princess_y)
 end
 
-#bot postion by cordinates X-axis and Y-axis
+# bot postion by cordinates X-axis and Y-axis
 def get_bot_position(grid)
-  bot = []
-  grid.each_index do |g|
-    x = grid[g].index 'm'
-    bot[0] = g
-    bot[1] = x
-    break if !x.nil?
+  grid.each_index do |x|
+    @row = x
+    @column = grid[x].index 'm'
+    break unless @column.nil?
   end
-  raise "Position of bot not found" if bot.empty?
-  return bot[0], bot[1]
+  raise 'Position of bot not found' if @column.nil?
+  return @row, @column
 end
 
-#princess postion by cordinates X-axis and Y-axis
+# princess postion by cordinates X-axis and Y-axis
 def get_princess_position(grid)
-  princess = []
-  grid.each_index do |g|
-    x = grid[g].index 'p'
-    princess[0] = g
-    princess[1] = x
-    break if !x.nil?
+  grid.each_index do |x|
+    @row = x
+    @column = grid[x].index 'p'
+    break unless @column.nil?
   end
-  raise "Position of princess not found" if princess.empty?
-  return princess[0], princess[1]
+  raise 'Position of princess not found' if @column.nil?
+  return @row, @column
 end
 
-#moving towards princess
-def bot_moving_towards_princess(b_x, b_y, p_x, p_y)
-  if b_x == p_x && b_y == p_y
-    return
-  else
-    if b_y < p_y
-      puts "Right"
-      b_y += 1
-    elsif b_y > p_y
-      puts "LEFT"
-      b_y -= 1
-    elsif b_x < p_x
-      puts "DOWN"
-      b_x += 1
-    elsif b_x > p_x
-      puts "UP"
-      b_x -= 1
-    end
-    bot_moving_towards_princess(b_x, b_y, p_x, p_y)
+# moving towards princess and recursive call
+def bot_moving_towards_princess(bot_x, bot_y, princess_x, princess_y)
+  return if bot_x == princess_x && bot_y == princess_y
+  if bot_y < princess_y
+    puts 'Right'
+    bot_y += 1
+  elsif bot_y > princess_y
+    puts 'LEFT'
+    bot_y -= 1
+  elsif bot_x < princess_x
+    puts 'DOWN'
+    bot_x += 1
+  elsif bot_x > princess_x
+    puts 'UP'
+    bot_x -= 1
   end
+  bot_moving_towards_princess(bot_x, bot_y, princess_x, princess_y)
 end
 
-#m denote bot and p denote princess inside grid
+# m denote bot and p denote princess inside grid
 n = 5
-grid = [['-','-','-','-','_'],['-','m','-','-','-'],['-','-','-','-','-'],['-','-','-','-','-'],['-','-','-','p','-']]
+grid = [['-', '-', '-', '-', '_'],
+        ['-', 'm', '-', '-', '-'],
+        ['-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-'],
+        ['_', '-', '-', 'p', '-']]
 
-displayPathtoPrincess(5, grid)
+display_path_to_princess(n, grid)
